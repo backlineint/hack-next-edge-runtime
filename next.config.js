e19273b577e159748d938778a88bc89d52b06f2c
Brian Perry <brian.perry@pantheon.io>
2022-10-27 13:26:35 -0500

    Enable edge runtime

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  experimental: {
+    runtime: "experimental-edge", // 'node.js' (default) | experimental-edge
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
